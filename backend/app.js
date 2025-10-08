9dfc0e4759426e0d5e34d731f046bdf225f6a3f3
Blackeye6941 <leok37316@gmail.com>
2025-10-08 14:24:18 +0000

    Express js added

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+	res.send("<h1>SkillSprint Backend</h1>");
+})
+
+app.listen(PORT, () => {
+	console.log(`Server running on http://localhost:${PORT}`);
+})
