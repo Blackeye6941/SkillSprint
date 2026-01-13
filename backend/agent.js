2d9978c3bb0d1e10c4779476b9f0339297aa72fc
Blackeye6941 <leok37316@gmail.com>
2026-01-13 15:32:18 +0000

    Prompt modified

--- backend/agent.js
+++ backend/agent.js
@@ -5,7 +5,7 @@ const { formSchema } = require("./utils/formSchema.js");
 	const prompt = "Create me a roadmap for a 2 day agentic ai course including frameworks used. i have some python knowledge. i need mini project every day and a main big project at the end of the course";
 	const res = await model.models.generateContent({
 		model:"gemini-2.5-flash",
-		contents:`You are an excellent roadmap generator. Provide the user a good roadmap according to the prompt provided here ${prompt}. You must provide the resources for each day and a 100-200 word description for him to understand the concepts. You can also add task to be done each day on that project. provide an id for each roadmap. You can also add weekly projects and instructions for the user to practically understand the topics covered each week or each 1/2th day of the course. Provide direct links to youtube and official docs for each day. Links or urls needed`,
+		contents:`You are an excellent roadmap generator. Provide the user a good roadmap according to the prompt provided here ${prompt}. You must provide the resources for each day and a 100-200 word description for him to understand the concepts. You can also add task to be done each day on that project. provide a unique integer id for each roadmap. You can also add weekly projects and instructions for the user to practically understand the topics covered each week or each 1/2th day of the course. Provide direct links to youtube and official docs for each day. Links or urls needed`,
 		config: {
 			responseMimeType: "application/json",
 			responseSchema: formSchema
