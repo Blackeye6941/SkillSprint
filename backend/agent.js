529ae86d6f63c183888ca25adcec463b803a5cee
Blackeye6941 <leok37316@gmail.com>
2026-01-19 16:27:53 +0000

    Added route

--- backend/agent.js
+++ backend/agent.js
@@ -1,8 +1,7 @@
 const model = require("./model.js");
 const { formSchema } = require("./utils/formSchema.js");
 
-(async function main(){
-	const prompt = "Create me a roadmap for a 2 day agentic ai course including frameworks used. i have some python knowledge. i need mini project every day and a main big project at the end of the course";
+async function generateRoadmap({prompt}){
 	const res = await model.models.generateContent({
 		model:"gemini-2.5-flash",
 		contents:`You are an excellent roadmap generator. Provide the user a good roadmap according to the prompt provided here ${prompt}. You must provide the resources for each day and a 100-200 word description for him to understand the concepts. You can also add task to be done each day on that project. provide a unique integer id for each roadmap. You can also add weekly projects and instructions for the user to practically understand the topics covered each week or each 1/2th day of the course. Provide direct links to youtube and official docs for each day. Links or urls needed`,
@@ -12,5 +11,6 @@ const { formSchema } = require("./utils/formSchema.js");
 		}
 	});
 	console.log(res.text);
-}
-)();
+};
+
+module.exports = {generateRoadmap};
--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const {generateRoadmap} = require('./agent.js');
 const app = express();
 const PORT = 3000;
 
@@ -6,7 +7,12 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
 	res.send("<h1>SkillSprint Backend</h1>");
-})
+});
+
+app.post('/generate', (req, res) => {	
+	const prompt = req.body;
+	generateRoadmap(prompt);
+});
 
 app.listen(PORT, () => {
 	console.log(`Server running on http://localhost:${PORT}`);
